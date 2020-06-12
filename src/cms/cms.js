92cb7661bfff47ec693e44ae59c24966bdadc240
dimitriosweb <dimitrios.web@outlook.com>
2020-06-12 18:24:04 +0200

    update services pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,8 +4,10 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import ServicePagePreview from './preview-templates/ServicePagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('services', ServicePagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
