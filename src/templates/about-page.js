cb0c95c306b8b476d07488a7b5851e4695ca7a40
dimitriosweb <dimitrios.web@outlook.com>
2020-06-19 11:57:58 +0200

    update about page template

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,22 +2,30 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Features from '../components/Features'
+import Testimonials from '../components/Testimonials'
 import Content, { HTMLContent } from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const AboutPageTemplate = ({ title, intro, testimonials, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
     <section className="section section--gradient">
       <div className="container">
         <div className="columns">
-          <div className="column is-10 is-offset-1">
+          <div className="column is-7 is-offset-1">
             <div className="section">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
               <PageContent className="content" content={content} />
             </div>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            {intro && (<Features gridItems={intro.blurbs} />)}
+            {testimonials && (<Testimonials testimonials={testimonials} />)}
           </div>
         </div>
       </div>
@@ -27,6 +35,10 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
+  testimonials: PropTypes.array,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
@@ -40,6 +52,8 @@ const AboutPage = ({ data }) => {
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
+        intro={post.frontmatter.intro}
+        testimonials={post.frontmatter.testimonials}
       />
     </Layout>
   )
@@ -57,6 +71,24 @@ export const aboutPageQuery = graphql`
       html
       frontmatter {
         title
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+        testimonials {
+          author
+          quote
+        }
       }
     }
   }
