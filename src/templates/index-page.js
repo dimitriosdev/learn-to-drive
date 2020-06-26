210614288412955a013e49159e498a1743c2fe6c
dimitriosweb <dimitrios.web@outlook.com>
2020-06-26 12:48:02 +0200

    add all services home page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -86,6 +86,13 @@ export const IndexPageTemplate = ({
                   </h2>
                   <p className="is-size-5">{pricing && pricing.description}</p>
                   <Pricing data={pricing.plans} />
+                  <div className="columns">
+                    <div className="column is-12 has-text-centered">
+                      <Link className="btn" to="/services">
+                        Όλες οι υπηρεσίες
+                    </Link>
+                    </div>
+                  </div>
                   <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
                       Νέα
